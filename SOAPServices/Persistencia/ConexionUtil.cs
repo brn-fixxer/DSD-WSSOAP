df7f24e8ed00963825ebe491fd0721728cf62a07
brn-fixxer <brn.chiappe@gmail.com>
2013-09-17 19:31:25 -0500

    primer commit

---
+++ SOAPServices/Persistencia/ConexionUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOAPServices.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            return "Data Source=87203c92-06ee-4ece-bf8d-a23c017e5e25.sqlserver.sequelizer.com; Initial Catalog=db87203c9206ee4ecebf8da23c017e5e25; User Id=uobppvrdvszkelqr; Password=TLqzzfzVBdacPLVRtbCvqkBZVzCQY4Yy67MeBrWRiqjGYMBn4odi7rzHixL8KS5F;";
+        }
+    }
+}
